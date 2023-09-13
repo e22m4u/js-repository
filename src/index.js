b8663d21bacc367c1c4208c445f597d362bcd83e
e22m4u <e22m4u@gmail.com>
2023-09-13 14:06:48 +0300

    fix: removes "./service/index.js" export

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ export * from './utils/index.js';
 export * from './errors/index.js';
 export * from './filter/index.js';
 export * from './adapter/index.js';
-export * from './service/index.js';
 export * from './relations/index.js';
 export * from './definition/index.js';
 export * from './repository/index.js';
