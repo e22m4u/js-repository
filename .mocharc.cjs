2bcbb00031ce47acf08633ed3213b0dbcc808638
e22m4u <e22m4u@yandex.ru>
2024-09-25 22:23:44 +0300

    chore: updates dependencies

--- .mocharc.cjs
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  extension: ['js', 'ts'],
-  spec: 'src/**/*.spec.{js,ts}',
-  require: [path.join(__dirname, 'mocha.setup.js')],
-}
--- mocha.setup.js
+++
@@ -1,6 +0,0 @@
-process.env['NODE_ENV'] = 'test';
-
-// ts loader
-import {register} from 'node:module';
-import {pathToFileURL} from 'node:url';
-register('ts-node/esm', pathToFileURL('./'));
