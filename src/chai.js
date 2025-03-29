e5e9445eaf22302757a891227652523f7b4f8883
e22m4u <e22m4u@yandex.ru>
2025-03-30 02:25:25 +0300

    chore: removes chai-subset dependenciy

--- src/chai.js
+++ src/chai.js
@@ -1,11 +1,9 @@
 import * as chaiModule from 'chai';
 import chaiSpies from 'chai-spies';
-import chaiSubset from 'chai-subset';
 import chaiAsPromised from 'chai-as-promised';
 const chai = {...chaiModule};
 
 chaiSpies(chai, chai.util);
-chaiSubset(chai, chai.util);
 chaiAsPromised(chai, chai.util);
 
 export {chai};
